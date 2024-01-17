f586a87edeaabf2f68877439ab38cec1c6415dce
mostofa khan <mdmostofakhan1972@gmail.com>
2024-01-17 18:35:58 +0600

    third

--- src/componets/Banner/Banner.jsx
+++ src/componets/Banner/Banner.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 
 const Banner = () => {
-    return (
-      <div className=''>
-          <div className='h-screen mt-8 flex justify-center items-center bg-cover
-        bg-[url(https://images.unsplash.com/photo-1582005450386-52b25f82d9bb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)]
-         bg-no-repeat'
-      >     
-       <div className='w-[60%] text-center'>
-         <p className='text-white text-6xl  text-center font-bold'>The quick and best easiest way to bring your new business ideas</p>
-         <p className='mt-8 text-2xl  text-white'>Amet minim mollit non desrunt ullgmcoest sit qliqua dolar do amet sing. Veelit officia consequat duis enim velit mollit. Exercitation veniam consequot sunt nostrud amet.</p>
-         <button className='bg-[rgb(255,208,78)] border text-white text-xl  w-32 p-2 rounded-lg text-center mt-6 '>Join Now</button>
-        
-       </div>
-       </div>
+  return (
+    <div className='p-4  mt-6'>
+      <div
+        className='h-screen flex justify-center items-center bg-cover bg-no-repeat'
+        style={{
+          backgroundImage: 'url(https://images.unsplash.com/photo-1582005450386-52b25f82d9bb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)',
+        }}
+      >
+        <div className='md:w-1/2 text-center'>
+          <p className='text-white text-3xl md:text-5xl font-bold'>The quick and best easiest way to bring your new business ideas</p>
+          <p className='mt-4 text-xl md:text-2xl text-white'>Amet minim mollit non desrunt ullgmcoest sit qliqua dolar do amet sing. Veelit officia consequat duis enim velit mollit. Exercitation veniam consequot sunt nostrud amet.</p>
+          <button className='bg-[rgb(255,208,78)] border text-white text-lg md:text-xl w-full md:w-32 p-2 rounded-lg text-center mt-6'>Join Now</button>
+        </div>
       </div>
-    );
-};  
+    </div>
+  );
+};
 
 export default Banner;
--- src/componets/Navbar/Navbar.jsx
+++ src/componets/Navbar/Navbar.jsx
@@ -1,51 +1,72 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Icon } from '@iconify/react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Icon } from "@iconify/react";
 
 const Navbar = () => {
-   const text = ['Home', 'Features', 'About Us', 'services', 'Blog', 'Contact']
-   
-   const [isMenuOpen, setIsMenuOpen] = useState(false)
-   
-   function  onToggleMenu () {
-       setIsMenuOpen(!isMenuOpen);
-   }
+  const text = ["Home", "Features", "About Us", "services", "Blog", "Contact"];
 
-    return (
-        <div className='mt-8 mx-auto'>
-            <div className='flex justify-around items-center'>      
-              <div className='flex justify-center items-center gap-4'>
-             <div className='relative'>
-             <div className='flex justify-center items-center rotate-45 bg-[rgb(33,161,234)] w-20 h-20 p-3 rounded-lg '>
-            
-            </div>
-            <h1 className='text-5xl text-white absolute top-[16%] left-[30%]  font-bold'>p</h1>
-             </div>
-               <h1 className='text-5xl text-black font-bold'>Primrose</h1>  
-              </div>
+  const [isMenuOpen, setIsMenuOpen] = useState(null);
 
-               <div className={`md:static  absolute bg-white  top-${isMenuOpen ? '-100' : '[-100%]'} md:w-auto w-full flex items-center px-5`}>
-               <Icon onClick={onToggleMenu} className='text-3xl cursor-pointer absolute right-2 bottom-0   md:hidden' icon={isMenuOpen ? 'mingcute:close-fill' : 'ic:round-menu'} />
-               <div className='flex md:flex-row flex-col md:items-center md:gap-10 gap-6 text-2xl font-medium text-black'>
-                   {
-                    text?.map((text,index) => (
-                        <div key={index}> 
-                            <Link className='hover:text-[rgb(33,161,234)] transition-all duration-300' to= {`/${text}`}>{text}</Link>
-                           
-                        </div>
-                    ))
-                   }
-                   
-               </div>
-               </div>
-               <div className='flex items-center gap-6 '>
-                    <button className='border py-1 px-5 rounded-lg text-white bg-[rgb(33,161,234)]'>Sign in</button>
-                    <Icon onClick={onToggleMenu} className='text-3xl cursor-pointer md:hidden' icon={isMenuOpen ? 'mingcute:close-fill' : 'ic:round-menu'} />
-                   </div>
-            </div>
-       
+  return (
+    <div className="mt-8">
+      <div className="flex justify-around items-center">
+        <div className="flex justify-center items-center gap-4">
+          <div className="relative">
+            <div className="flex justify-center items-center rotate-45 bg-[rgb(33,161,234)] lg:w-20 w-14 h-14 lg:h-20 p-3 rounded-lg "></div>
+            <h1 className="lg:text-5xl text-4xl text-white absolute lg:top-[16%] top-[8%] left-[30%]  font-bold">
+              p
+            </h1>
+          </div>
+          <h1 className="lg:text-5xl text-3xl text-black font-bold">
+            Primrose
+          </h1>
         </div>
-    );
+
+        <div className={` bg-white flex items-center px-5`}>
+          <div className="md:block hidden">
+            <div className="flex md:flex-row flex-col md:items-center md:gap-10 gap-6 text-2xl font-medium text-black">
+              {text?.map((text, index) => (
+                <div key={index}>
+                  <Link
+                    className="hover:text-[rgb(33,161,234)] transition-all duration-300"
+                    to={`/${text}`}
+                  >
+                    {text}
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div
+            className={`${
+              isMenuOpen ? "block lg:hidden" : "hidden"} 
+              flex flex-col absolute top-20 shadow-2xl bg-white p-5 rounded-md w-full gap-4 text-2xl font-medium text-black`}
+          >
+            {text?.map((text, index) => (
+              <div key={index}>
+                <Link
+                  className="hover:text-[rgb(33,161,234)] transition-all duration-300"
+                  to={`/${text}`}
+                >
+                  {text}
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex items-center gap-6 ">
+          <button className="border py-1 px-5 rounded-lg text-white bg-[rgb(33,161,234)]">
+            Sign in
+          </button>
+          <Icon
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            className="text-3xl lg:hidden cursor-pointer"
+            icon={isMenuOpen ? "mingcute:close-fill" : "ic:round-menu"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
