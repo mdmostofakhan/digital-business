83d1fa3e1133a34bce19fbecee4c37ae32d030ec
mostofa khan <mdmostofakhan1972@gmail.com>
2024-01-24 00:35:45 +0600

    business

--- src/Home/pages/NewsSection.jsx
+++ src/Home/pages/NewsSection.jsx
@@ -11,33 +11,33 @@ const NewsSection = () => {
   }, []);
 
   return (
-    <div className="w-[70%] mx-auto ">
-      <div className="mt-28 gap-5 grid grid-cols-2 justify-center items-center">
-        <div className=" ">
+    <div className=" container mx-auto ">
+      <div className="mt-28 gap-5 grid grid-cols-2 justify-between">
+        <div className="w-full h-full">
           <h1 className="text-4xl mb-20 font-bold">Our Blog News</h1>
-          <div className="w-full">
+          <div className="h-5/6 w-full">
             <img
-              className="bg-cover bg-no-repeat w-full mb-12 h-auto"
+              className="object-cover shadow-2xl h-full w-full bg-no-repeat "
               src="https://images.unsplash.com/photo-1552664730-d307ca884978?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt=""
             />
           </div>
         </div>
 
-        <div className="">
+        <div className="mt-28">
           <h1 className="text-3xl  font-bold text-[#0e9ce6]">
             Recommended Blog
           </h1>
           {news?.map((newer, index) => (
             <div key={index} className="flex justify-center items-center gap-5">
               <img
-                className="  w-1/5  rounded-lg "
+                className="  w-1/6 object-cover rounded-lg "
                 src={newer.image}
                 alt=""
               />
               <div className="">
-                <h1 className="text-3xl mt-16  w-[60%] font-semibold">{newer.title}</h1>
-                <p className=" text-xl w-[80%] mt-4 opacity-65">{newer.text}</p>
+                <h1 className="text-2xl mt-12  w-[45%] font-semibold">{newer.title}</h1>
+                <p className=" text-xl w-[75%] mt-2 opacity-65">{newer.text}</p>
               </div>
             </div>
           ))}
